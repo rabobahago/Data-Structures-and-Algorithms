113233356fdd50f09c012e94a468bb79721c3a2e
rabobahago <rabobahago@gmail.com>
2022-06-01 09:14:57 +0100

    palindrome check solution added

---
+++ string/palindromeCheck.js
@@ -0,0 +1,9 @@
+function isPalindrome(string) {
+  let str = string.split("");
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
