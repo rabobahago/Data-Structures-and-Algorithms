770a2dda6924d9701674c6b1a4e8f01c636f60b8
rabobahago <rabobahago@gmail.com>
2022-06-17 04:54:43 +0100

    quick select solution added

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-function searchForRange(array, target) {
-  let finalRange = [-1, -1];
-  alteredBinarySearch(array, target, 0, array.length - 1, finalRange, true);
-  alteredBinarySearch(array, target, 0, array.length - 1, finalRange, false);
-  return finalRange;
+function quickselect(array, k) {
+  //because indices always start from 0, k smallest will always start from index - 1
+  let position = k - 1;
+  return quickselectHelper(array, 0, array.length-1; position)
 }
-function alteredBinarySearch(array, target, left, right, finalRange, goLeft) {
-  if (left > right) {
-    return;
-  }
-  let middle = Math.floor((right + left) / 2);
-  if (array[middle] < target) {
-    alteredBinarySearch(array, target, middle + 1, right, finalRange, goLeft);
-  } else if (array[middle] > target) {
-    alteredBinarySearch(array, target, left, middle - 1, finalRange, goLeft);
-  } else {
-    if (goLeft) {
-      if (middle === 0 || array[middle - 1] !== target) {
-        finalRange[0] = middle;
-      } else {
-        alteredBinarySearch(
-          array,
-          target,
-          left,
-          middle - 1,
-          finalRange,
-          goLeft
-        );
+function quickselectHelper(array, startInd, endInd, postion){
+  while(true){
+    if(startInd > endInd){
+      throw new Error('your algorithm should never arrive here')
+    }
+    let pivotInd = startInd;
+    let leftInd = startInd + 1;
+    let rightInd = endInd;
+    //while left index is still less or equal to right index
+    while(leftInd <= rightInd){
+      //if this condition happen then it mean we need to swap
+     if(array[leftInd] > array[pivotInd] && array[rightInd] < array[pivotInd]){
+       swap(leftInd, rightInd, array)
+     } 
+      if(array[leftInd] <= array[pivotInd]){
+        leftInd++
       }
-    } else {
-      if (middle == array.length - 1 || array[middle + 1] !== target) {
-        finalRange[1] = middle;
-      } else {
-        alteredBinarySearch(
-          array,
-          target,
-          middle + 1,
-          right,
-          finalRange,
-          goLeft
-        );
+      if(array[rightInd] >= array[pivotInd]){
+        rightInd--
       }
     }
+    swap(pivotInd, rightInd, array);
+    if(rightInd === position){
+      return array[rightInd]
+    }else if(rightInd < position){
+      startInd = rightInd + 1;
+    }else{
+      endInd = rightInd - 1;
+    }
   }
+}
+function swap(one, two, array){
+  let temp = array[one];
+  array[one] = array[two];
+  array[two] = temp;
 }
---
+++ searching/quickselect.js
@@ -0,0 +1,44 @@
+function quickselect(array, k) {
+  //because indices always start from 0, k smallest will always start from index - 1
+  let position = k - 1;
+  return quickselectHelper(array, 0, array.length - 1, position);
+}
+function quickselectHelper(array, startInd, endInd, position) {
+  while (true) {
+    if (startInd > endInd) {
+      throw new Error("your algorithm should never arrive here");
+    }
+    let pivotInd = startInd;
+    let leftInd = startInd + 1;
+    let rightInd = endInd;
+    //while left index is still less or equal to right index
+    while (leftInd <= rightInd) {
+      //if this condition happen then it mean we need to swap
+      if (
+        array[leftInd] > array[pivotInd] &&
+        array[rightInd] < array[pivotInd]
+      ) {
+        swap(leftInd, rightInd, array);
+      }
+      if (array[leftInd] <= array[pivotInd]) {
+        leftInd++;
+      }
+      if (array[rightInd] >= array[pivotInd]) {
+        rightInd--;
+      }
+    }
+    swap(pivotInd, rightInd, array);
+    if (rightInd === position) {
+      return array[rightInd];
+    } else if (rightInd < position) {
+      startInd = rightInd + 1;
+    } else {
+      endInd = rightInd - 1;
+    }
+  }
+}
+function swap(one, two, array) {
+  let temp = array[one];
+  array[one] = array[two];
+  array[two] = temp;
+}
