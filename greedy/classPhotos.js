87c54f5a4d2eabd8d13bbde57e2c9feef1287008
rabobahago <rabobahago@gmail.com>
2022-06-19 23:43:05 +0100

    class photos solution added

--- greedy/classPhotos.js
+++ greedy/classPhotos.js
@@ -3,15 +3,19 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
   redShirtHeights.sort((a, b) => b - a);
   blueShirtHeights.sort((a, b) => b - a);
   //take the first element of each array to determine a blue or red shift will come first
-  let firstOrSecond = redShirtHeights[0] < blueShirtHeights[0] ? "RED" : "BLUE";
+  let firstOrSecond = redShirtHeights[0] > blueShirtHeights[0] ? "RED" : "BLUE";
   //loop through the two array
   for (let i = 0; i < redShirtHeights.length; i++) {
     let redElem = redShirtHeights[i];
     let blueElem = blueShirtHeights[i];
     if (firstOrSecond === "RED") {
-      if (redElem >= blueElem) return false;
-    } else if (blueElem >= redElem) return false;
+      //when red condition is not met return false
+      if (redElem <= blueElem) return false;
+    } else if (firstOrSecond === "BLUE") {
+      //when blue condition is not met return false
+      if (blueElem <= redElem) return false;
+    }
   }
-
+  //else return true
   return true;
 }
