680e965bbb3f400928f7c23b6828dfe231296c00
rabobahago <rabobahago@gmail.com>
2022-06-22 02:22:03 +0100

    find close value solution added

---
+++ binarySearchtree/findTheCloseValue.js
@@ -0,0 +1,19 @@
+function findClosestValueInBst(tree, target) {
+  return helperClosesValueInBst(tree, target, tree.value);
+}
+
+function helperClosesValueInBst(tree, target, closest) {
+  let currentNode = tree;
+  while (currentNode !== null) {
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value;
+    } else if (target < currentNode.value) {
+      currentNode = currentNode.left;
+    } else if (target > currentNode.value) {
+      currentNode = currentNode.right;
+    } else {
+      break;
+    }
+  }
+  return closest;
+}
