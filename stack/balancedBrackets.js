4e8e4c218e4ae078d3cb99e0b49b3958f7ebf3a9
rabobahago <rabobahago@gmail.com>
2022-06-25 16:25:49 +0100

    balanced bracket solution added

--- stack/balancedBrackets.js
+++ stack/balancedBrackets.js
@@ -5,23 +5,37 @@ function balancedBrackets(string) {
   //2) when your stack is not empty but character is finish
   //2) when character doesn't match the pop one
   let stack = [];
+  //the opening brackets
   const openingBrackets = "([{";
+  //the closing brackets
   const closingBrackets = ")]}";
+  // matching Brackets
   const matchingBrackets = { ")": "(", "]": "[", "}": "{" };
+  //loop through the string
   for (let i = 0; i < string.length; i++) {
     let char = string[i];
+    //check if the character is in openingBrackets
     if (openingBrackets.includes(char)) {
+      //push the opening bracket
       stack.push(char);
+      //check if the character is in closingBrackets
     } else if (closingBrackets.includes(char)) {
+      //then check again if the length of the stack is empty
       if (stack.length === 0) {
+        //return false
         return false;
       }
+      //then check again if the last character in the pop is equal to matchingBrackets
       if (stack[stack.length - 1] === matchingBrackets[char]) {
-        return stack.pop();
+        //if so, pop
+        stack.pop();
       } else {
+        //else return false
         return false;
       }
     }
   }
+  //check if the length of the stack is empty after everything
+  //if empty return true else return false
   return stack.length === 0;
 }
