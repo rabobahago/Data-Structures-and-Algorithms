3f7e7eccf0aeb3c1612f3d4af1416ffe61ad3eb7
rabobahago <rabobahago@gmail.com>
2022-06-28 04:59:35 +0100

    staircase traversal solution added

--- recursion/stairTraversal.js
+++ recursion/stairTraversal.js
@@ -1,3 +1,4 @@
+//O(n * K) time | O(n) space
 function staircaseTraversal(height, maxSteps) {
   //call callback numberOfWaysToTop with height of staircase and maxStep;
   return numberOfWaysToTop(height, maxSteps, { 0: 1, 1: 1 });
@@ -5,7 +6,9 @@ function staircaseTraversal(height, maxSteps) {
 function numberOfWaysToTop(height, maxSteps, memoize) {
   //if height is in the object memoize it
   if (height in memoize) return memoize[height];
+  //declare  numberOfways
   let numberOfWays = 0;
+  //for loop
   for (let step = 1; step < Math.min(maxSteps, height) + 1; step++) {
     numberOfWays += numberOfWaysToTop(height - step, maxSteps, memoize);
   }
