35bd69bd8936e51f7b5ad6fc14a6145f40b28f16
rabobahago <rabobahago@gmail.com>
2022-07-02 23:16:38 +0100

    algo casts parts of algorithms and data structure added

--- index.js
+++ index.js
@@ -1,93 +1,12 @@
-// Do not edit the class below except for
-// the insert, contains, and remove methods.
-// Feel free to add new properties and methods
-// to the class.
-class BST {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-  // average O(log(n)) time | O(1) space
-  //worst O(n) time | O(1)
-  insert(value) {
-    let currentNode = this;
-    while (true) {
-      if (value < currentNode.value) {
-        if (currentNode.left === null) {
-          currentNode.left = new BST(value);
-          break;
-        } else {
-          currentNode = currentNode.left;
-        }
-      } else {
-        if (currentNode.left === null) {
-          currentNode.right = new BST(value);
-          break;
-        } else {
-          currentNode = currentNode.right;
-        }
-      }
-    }
+function fibb(n) {
+  if (n <= 2) return n;
+  return fibb(n - 1) + fibb(n - 2);
+}
+console.log(fibb(20));
 
-    return this;
-  }
-
-  contains(value) {
-    let currentNode = this;
-    while (currentNode !== null) {
-      if (value < currentNode.value) {
-        currentNode = currentNode.left;
-      } else if (value > currentNode.value) {
-        currentNode = currentNode.right;
-      } else {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  remove(value, parentNode = null) {
-    let currentNode = this;
-    while (currentNode !== null) {
-      if (value < currentNode.value) {
-        parentNode = currentNode;
-        currentNode = currentNode.left;
-      } else if (value > currentNode.value) {
-        parentNode = currentNode;
-        currentNode = currentNode.right;
-      } else {
-        if (currentNode.left !== null && currentNode.right !== null) {
-          currentNode.value = currentNode.right.getMinValue();
-          currentNode.right.remove(currentNode.value, currentNode);
-        } else if (parentNode === null) {
-          if (currentNode.left !== null) {
-            currentNode.value = currentNode.right.value;
-            currentNode.right = currentNode.left.right;
-            currentNode.left = currentNode.left.left;
-          } else if (currentNode.right !== null) {
-            currentNode.value = currentNode.right.value;
-            currentNode.left = currentNode.right.left;
-            currentNode.right = currentNode.right.right;
-          } else {
-          }
-        } else if (parentNode.left === currentNode) {
-          parentNode.left =
-            currentNode.left !== null ? currentNode.left : currentNode.right;
-        } else if (parentNode.right === currentNode) {
-          parentNode.right =
-            currentNode.left !== null ? currentNode.left : currentNode.right;
-        }
-        break;
-      }
-    }
-    return this;
-  }
-  getMinValue() {
-    let currentNode = this;
-    while (currentNode !== null) {
-      currentNode = currentNode.left;
-    }
-    return currentNode.value;
-  }
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+  else if (i % 5 === 0) console.log("Buzz");
+  else if (i % 3 === 0) console.log("Fizz");
+  else console.log(i);
 }
