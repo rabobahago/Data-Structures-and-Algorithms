993fa8cd5adc182d1dbdff54051d679a4eecc652
rabobahago <rabobahago@gmail.com>
2022-07-12 12:41:17 +0100

    remove kth node from end solution added

---
+++ linkedList/removeKthNodeFromEnd.js
@@ -0,0 +1,20 @@
+// O(n) time | O(1) space
+function removeKthNodeFromEnd(head, k) {
+  let counter = 1;
+  let first = head;
+  let second = head;
+  while (counter <= k) {
+    second = second.next;
+    counter++;
+  }
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+  while (second.next !== null) {
+    second = second.next;
+    first = first.next;
+  }
+  first.next = first.next.next;
+}
