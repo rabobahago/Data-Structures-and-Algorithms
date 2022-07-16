3721b74ae28cfd725d4587ac10212a5b0fcba048
rabobahago <rabobahago@gmail.com>
2022-07-16 10:59:58 +0100

    find loop solution added

---
+++ linkedList/findLoop.js
@@ -0,0 +1,22 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function findLoop(head) {
+  let first = head.next;
+  let second = head.next.next;
+  while (first !== second) {
+    first = first.next;
+    second = second.next.next;
+  }
+  first = head;
+  while (first !== second) {
+    first = first.next;
+    second = second.next;
+  }
+  return first;
+}
