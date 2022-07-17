8e38865c9124e7f4cd26e38f78d18bebe0ede118
rabobahago <rabobahago@gmail.com>
2022-07-17 12:20:55 +0100

    return string at character solution added

---
+++ challenges/returnStringAtCharacter.js
@@ -0,0 +1,15 @@
+const returnStringAtCharacter = (string, k) => {
+  //   let splitArray = string.split("\n");
+  //   const returnString = splitArray.slice(k - 1, k);
+  //   return returnString.join("");
+  return string
+    .split("\n")
+    .slice(k - 1, k)
+    .join();
+};
+console.log(returnStringAtCharacter("apples\nmilk\nbread\nfish\n", 2));
+//milk
+console.log(
+  returnStringAtCharacter("Beverly Hills\nSagaponack\nSanta Monica\n", 1)
+);
+//Beverly Hills
