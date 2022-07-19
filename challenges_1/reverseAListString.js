885c7f85ea5fdf2fdd4047ae0da61333eadfe1ce
rabobahago <rabobahago@gmail.com>
2022-07-19 17:49:05 +0100

    challenges two added

--- challenges_1/reverseAListString.js
+++ challenges_1/reverseAListString.js
@@ -2,3 +2,4 @@ const reverseAListString = (array) => {
   return array.reverse();
 };
 console.log(reverseAListString(["Cat", "Dog", "Skunk", "Bird"]));
+console.log(reverseAListString(["owl", "ferret", "Mouse!", "Eagle"]));
