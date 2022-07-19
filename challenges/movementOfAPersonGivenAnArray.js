f5d5f93515a0b131ea4a61902fc0255edc12daf5
rabobahago <rabobahago@gmail.com>
2022-07-19 16:42:06 +0100

    movement of a person given an array solution added

---
+++ challenges/movementOfAPersonGivenAnArray.js
@@ -0,0 +1,15 @@
+const movementOfAPersonGivenAnArray = (array) => {
+  // the question starting from 0, 0
+  return array.reduce(
+    ([a, b], curr) => {
+      if (curr === "up") return [a, b + 1];
+      if (curr === "down") return [a, b - 1];
+      if (curr === "left") return [a - 1, b];
+      if (curr === "right") return [a + 1, b];
+    },
+    [0, 0]
+  );
+};
+console.log(
+  movementOfAPersonGiven(["up", "up", "down", "left", "left", "right", "up"])
+);
