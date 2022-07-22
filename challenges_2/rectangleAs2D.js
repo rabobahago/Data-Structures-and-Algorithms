e34d1bbf058203bdabb4d873f4417c8bee54600d
rabobahago <rabobahago@gmail.com>
2022-07-22 16:34:17 +0100

    Return a Rectangle as a 2D array solution added

---
+++ challenges_2/rectangleAs2D.js
@@ -0,0 +1,48 @@
+// Return a Rectangle as a 2D array
+// You are given a two dimensional array containing two arrays of integer pairs as an argument. The first inner array pair represents the bottom left point of a rectangle, and the second inner array pair represents the top right point of the rectangle.
+
+// Return a two dimensional array representing the same rectangle but instead with the first inner array containing the the bottom left point of the rectangle and the second inner array containing the height and width of the rectangle.
+
+// Requirements
+// Must return a two dimensional array containing two arrays of integer pairs.
+// Example #1
+// solve([[10, 5], [23, 8]])
+// > [[10,5], [13, 3]]
+// Example #2
+// solve([[3, 9], [5, 15]])
+// > [[3, 9], [6, 2]]
+const rectangleAs2D = (array) => {
+  let result = [];
+  let v1,
+    w1 = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (i === 0) {
+      [v1, w1] = array[i];
+      result.push(array[i]);
+    } else {
+      const [x1, y1] = array[i];
+      result.push([Math.abs(x1 - v1), Math.abs(y1 - w1)]);
+    }
+  }
+  return result;
+};
+console.log(
+  rectangleAs2D([
+    [10, 5],
+    [23, 8],
+  ])
+);
+console.log(
+  rectangleAs2D([
+    [3, 9],
+    [5, 15],
+  ])
+);
+const solve = (intArray) => {
+  let x = intArray[0];
+  let y = [
+    Math.abs(intArray[0][0] - intArray[1][0]),
+    Math.abs(intArray[0][1] - intArray[1][1]),
+  ].sort((a, b) => a - b);
+  return [x, y];
+};
