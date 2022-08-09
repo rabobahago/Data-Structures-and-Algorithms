1c60844079c253a755783333d0d38ff025289f34
rabobahago <rabobahago@gmail.com>
2022-08-09 12:16:40 +0100

    grid traveler solution added

---
+++ dynamicProgramming/memoization/gridTraveler.js
@@ -0,0 +1,23 @@
+//time complexity O(2^n + m) and space of O(m + n)
+const gridTravelers = (m, n) => {
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  return gridTravelers(m - 1, n) + gridTravelers(m, n - 1);
+};
+console.log(gridTravelers(2, 3));
+console.log(gridTravelers(3, 3));
+
+//this is known as memoization of gridTraveler
+//the  complexity of O(m * n) and space complexity O(m + n)
+const gridTraveler = (m, n, memo = {}) => {
+  let key = m + ", " + n; //common gotchas in JavaScript
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  if (key in memo) return memo[key];
+  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+  return memo[key];
+};
+console.log(gridTraveler(2, 3));
+console.log(gridTraveler(3, 3));
+console.log(gridTraveler(10, 10));
+console.log(gridTraveler(20, 20));
