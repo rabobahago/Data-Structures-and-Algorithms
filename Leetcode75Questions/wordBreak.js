7dd12a2ee1ec7ffae735c58c9ec7179235e7e3d5
rabobahago <rabobahago@gmail.com>
2022-08-10 10:07:23 +0100

    word break problem solution added

---
+++ Leetcode75Questions/wordBreak.js
@@ -0,0 +1,38 @@
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+// Example 1:
+
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+// Example 2:
+
+// Input: s = "applepenapple", wordDict = ["apple","pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+// Note that you are allowed to reuse a dictionary word.
+// Example 3:
+
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: fals
+let wordBreak = (s, wordDict, memo = {}) => {
+  if (s in memo) return memo[s];
+  if (s.length === 0) return true;
+  for (let word of wordDict) {
+    const indexFound = s.indexOf(word);
+    if (indexFound === 0) {
+      let suffix = s.slice(word.length);
+      //console.log(suffix);
+      if (wordBreak(suffix, wordDict, memo) === true) {
+        return true;
+      }
+    }
+  }
+  memo[s] = false;
+  return false;
+};
+console.log(wordBreak("leetcode", ["leet", "code"]));
+console.log(wordBreak("applepenapple", ["apple", "pen"]));
+console.log(wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]));
