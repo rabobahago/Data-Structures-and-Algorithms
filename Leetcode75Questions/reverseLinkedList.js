98ce0a5c15db0755e4a185cb37b09ebb4a669e47
rabobahago <rabobahago@gmail.com>
2022-08-12 17:16:23 +0100

    reverse linked list problem solution added

---
+++ Leetcode75Questions/reverseLinkedList.js
@@ -0,0 +1,23 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Example 2:
+
+// Input: head = [1,2]
+// Output: [2,1]
+// Example 3:
+
+// Input: head = []
+// Output: []
+const reverseLinkedList = (head) => {
+  let prev = null;
+  let current = head;
+  while (current !== null) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
