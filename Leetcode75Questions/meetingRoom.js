57edef4c4991dfcb40c927bd2b2d4cc896f4af1a
rabobahago <rabobahago@gmail.com>
2022-08-12 16:06:05 +0100

    meeting room problem solution added

---
+++ Leetcode75Questions/meetingRoom.js
@@ -0,0 +1,36 @@
+//Meeting Room
+// Description
+// Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+// Example 1:
+
+// Input: intervals = [[0,30],[5,10],[15,20]]
+// Output: false
+// Example 2:
+
+// Input: intervals = [[7,10],[2,4]]
+// Output: true
+const meetingRoom = (intervals) => {
+  intervals.sort((a, b) => a[0] - b[0]);
+  //let result = [intervals[0]];
+  for (let i = 1; i < intervals.length; i++) {
+    let e = intervals[i - 1][1];
+    //console.log(intervals[i]);
+    let s = intervals[i][0];
+    if (e > s) return false;
+  }
+  return true;
+};
+console.log(
+  meetingRoom([
+    [0, 30],
+    [5, 10],
+    [15, 20],
+  ])
+);
+console.log(
+  meetingRoom([
+    [7, 10],
+    [2, 4],
+  ])
+);
