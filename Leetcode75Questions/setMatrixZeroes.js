3a32d1abb9269c3863a2a2ac62169831bf27d223
rabobahago <rabobahago@gmail.com>
2022-08-16 09:27:13 +0100

    set zeroes matrix problem solution added

---
+++ Leetcode75Questions/setMatrixZeroes.js
@@ -0,0 +1,39 @@
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+// You must do it in place.
+
+// Example 1:
+
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Example 2:
+
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+const setMatrixZeroes = (matrix) => {
+  let zeros = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        zeros.push([i, j]);
+      }
+    }
+  }
+  for (let i = 0; i < zeros.length; i++) {
+    let [row, col] = zeros[i];
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][col] = 0;
+    }
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix[row][j] = 0;
+    }
+  }
+};
+
+console.log(
+  setMatrixZeroes([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ])
+);
--- elementaryAlgo/setOperations.js
+++ elementaryAlgo/setOperations.js
@@ -20,3 +20,23 @@ const unionOfSet = (setA, setB) => {
   return union;
 };
 console.log(unionOfSet(new Set([1, 2, 5, 7, 8]), new Set([2, 4, 6, 8, 9])));
+//difference of setA and setB
+const differenceOfSet = (setA, setB) => {
+     for(let i of setB){
+        if(setA.has(i)){
+            setA.delete(i)
+        }
+     }
+     return setA
+}
+console.log(differenceOfSet(new Set([1, 2, 5, 7, 8]), new Set([2, 4, 6, 8, 9])));
+//subse of setA and setB
+const subSet = (setA, setB) => {
+    for(let i of setB){
+        if(!setA.has(i)){
+            return false
+        }
+    }
+    return true
+}
+console.log(subSet(new Set([1, 2, 5, 7, 8]), new Set([2, 4, 6, 8, 9])))
