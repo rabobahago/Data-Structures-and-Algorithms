629073002236292639ecbac6f05c05af6bb5facf
rabobahago <rabobahago@gmail.com>
2022-08-22 09:10:15 +0100

    print 2d array solution added

--- elementaryAlgo/alvin/total.js
+++ elementaryAlgo/alvin/total.js
@@ -12,3 +12,8 @@ console.log(total([3, 2, 8])); // 13
 console.log(total([-5, 7, 4, 6])); // 12
 console.log(total([7])); // 7
 console.log(total([])); // 0
+
+let obj = {
+  name: "right",
+  number: 3,
+};
