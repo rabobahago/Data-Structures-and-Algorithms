6dca6bc9803ece7945611a13c9267a18636d10a9
rabobahago <rabobahago@gmail.com>
2022-08-22 09:17:31 +0100

    total product solution added

---
+++ elementaryAlgo/alvin/makeMatrix.js
@@ -0,0 +1,25 @@
+// Write a function `makeMatrix(m, n, value)` that accepts three arguments. The function should return
+// a 2-dimensional array of height `m` and width `n` that contains the `value` as every element.
+const makeMatrix = (m, n, value) => {
+  let matrix = Array.from(Array(m), () => Array(n).fill(value));
+};
+console.log(makeMatrix(3, 5, null));
+// [
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ],
+//   [ null, null, null, null, null ]
+// ]
+
+console.log(makeMatrix(4, 2, "x"));
+// [
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ],
+//   [ 'x', 'x' ]
+// ]
+
+console.log(makeMatrix(2, 2, 0));
+// [
+//   [ 0, 0 ],
+//   [ 0, 0 ]
+// ]
