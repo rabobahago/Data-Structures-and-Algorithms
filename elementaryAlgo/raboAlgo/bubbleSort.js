b94ca54a01dafa520cdde6310b91560455e4b6e5
rabobahago <rabobahago@gmail.com>
2022-09-22 19:37:34 +0100

    comments added to bubble, insertion and quick sort

--- elementaryAlgo/raboAlgo/bubbleSort.js
+++ elementaryAlgo/raboAlgo/bubbleSort.js
@@ -1,6 +1,10 @@
 const bubbleSort = (arr) => {
+  //for every element compare with rest of elements
   for (let i = 0; i < arr.length; i++) {
+    //start loop till one element less the end of array because of  j + 1 maybe  undefined
+    //element i from the left is consider to be sorted already, that is why -i
     for (let j = 0; j < arr.length - 1 - i; j++) {
+      //if arr[j] > arr[j + 1] swap
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
