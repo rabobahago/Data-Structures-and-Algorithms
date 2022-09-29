70a7b2cbd1a5f278151b09c07983f61861ba3293
rabobahago <rabobahago@gmail.com>
2022-09-29 08:47:43 +0100

    same tree solution added

---
+++ Leetcode75Questions/sameTree.js
@@ -0,0 +1,11 @@
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+const isSameTree = (p, q) => {
+  if (p === null || q === null) return p === q;
+  return (
+    p.val === q.val &&
+    isSameTree(p.left, q.left) &&
+    isSameTree(p.right, q.right)
+  );
+};
