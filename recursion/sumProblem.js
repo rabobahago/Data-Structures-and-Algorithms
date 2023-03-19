55f9e05eabc957e57ac78c502165d3de94642723
rabobahago <rabobahago@gmail.com>
2023-03-19 13:15:06 +0100

    sum solution using recursion added

--- recursion/sumProblem.js
+++ recursion/sumProblem.js
@@ -2,16 +2,11 @@
 //The function should return total sum of all numbers.
 //The function must be recursive
 
-function sumRecursive(array) {
-  let sum = 0;
+function sum(array) {
   if (array.length === 0) return 0;
-  while (array.length !== 0) {
-    let arr = array.splice(0, 1);
-    sum += arr[0];
-  }
-
-  return sum;
+  let arr = array.slice(1);
+  return array[0] + sum(arr);
 }
-console.log(sumRecursive([9, 6, 6, 8]));
-console.log(sumRecursive([4]));
-console.log(sumRecursive([9, 6, 3]));
+console.log(sum([9, 6, 6, 8]));
+console.log(sum([4]));
+console.log(sum([9, 6, 3]));
