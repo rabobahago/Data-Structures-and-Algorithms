53115e52fe4a713543f5a105b315f3f17a43b89a
rabobahago <rabobahago@gmail.com>
2023-03-22 09:38:23 +0100

    shortest path graph rethink added

--- graph/graphPractice.js
+++ graph/graphPractice.js
@@ -200,3 +200,52 @@ console.log(
     4: ["3", "2"],
   })
 ); // -> 4
+
+// shortest path
+// Write a function, shortestPath, that takes in an array of edges
+// for an undirected graph and two nodes (nodeA, nodeB). The function
+// should return the length of the shortest path between A and B.
+// Consider the length as the number of edges in the path, not the number of nodes.
+// If there is no path between A and B, then return -1.
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = buildShortestGraph(edges);
+  let visited = new Set([nodeA]);
+  const queue = [[nodeA, 0]];
+  while (queue.length > 0) {
+    let [node, distance] = queue.shift();
+    if (node === nodeB) {
+      return distance;
+    }
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        queue.push([neighbor, distance + 1]);
+      }
+    }
+  }
+  return -1;
+};
+
+const buildShortestGraph = (edges) => {
+  let graph = {};
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) {
+      graph[a] = [];
+    }
+    if (!(b in graph)) {
+      graph[b] = [];
+    }
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+};
+const edgesShortest = [
+  ["w", "x"],
+  ["x", "y"],
+  ["z", "y"],
+  ["z", "v"],
+  ["w", "v"],
+];
+
+console.log(shortestPath(edgesShortest, "w", "z"));
