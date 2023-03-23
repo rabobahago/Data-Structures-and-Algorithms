93995727f2a531df7ca8b21bbfffcff980377f0f
rabobahago <rabobahago@gmail.com>
2023-03-23 11:35:28 +0100

    breadth first rethinked

---
+++ binaryTrees/breadthFirst.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+a.right = c;
+a.left = b;
+b.left = d;
+b.right = e;
+c.right = f;
+console.log(a);
+const breadthFirstValues = (root) => {
+  if (root === null) return [];
+  let result = [];
+  let queue = [root];
+  while (queue.length > 0) {
+    let element = queue.shift();
+    result.push(element.value);
+    if (element.left) queue.push(element.left);
+    if (element.right) queue.push(element.right);
+  }
+  return result;
+};
+console.log(breadthFirstValues(a));
