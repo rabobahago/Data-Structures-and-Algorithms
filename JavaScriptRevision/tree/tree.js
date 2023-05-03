7cb995e31c441ab9310da5b41359048eb4a5642c
rabobahago <rabobahago@gmail.com>
2023-05-03 13:08:07 +0100

    traverse post order tree method implemented

--- JavaScriptRevision/tree/tree.js
+++ JavaScriptRevision/tree/tree.js
@@ -54,6 +54,14 @@ class BSTree {
     }
     return memo;
 }
+traversePostOrder(node = this.root, memo = []) {
+  if (node !== null) {
+      this.traverseInOrder(node.left, memo);
+      this.traverseInOrder(node.right, memo);
+      memo.push(node.data);
+  }
+  return memo;
+}
   findMax() {
     let current = this.root;
     while (current.right !== null) {
