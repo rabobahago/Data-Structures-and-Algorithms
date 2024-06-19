9f3ab85545991d40a33e934f801cc62d1e3e9a4f
rabobahago <rabobahago@gmail.com>
2024-06-19 21:18:48 +0100

    all console.log statements removed in object and array files

--- JavaScriptRevision/objectAndArray.js
+++ JavaScriptRevision/objectAndArray.js
@@ -1,10 +1,3 @@
-let listOfNumbers = [2, 3, 5, 7, 11];
-console.log(listOfNumbers[2]);
-// → 5
-console.log(listOfNumbers[0]);
-// → 2
-console.log(listOfNumbers[2 - 1]);
-// → 3
 
 const menuConfig = {
   title: "Order",
